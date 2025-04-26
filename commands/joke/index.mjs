9c1298170c371919695afdfc3c2aa56c0dc1eb8e
BuildTools <unconfigured@null.spigotmc.org>
2025-04-26 16:53:58 -0400

    Initial commit

--- commands/joke/index.mjs
+++ commands/joke/index.mjs
@@ -6,6 +6,15 @@ screen.style.display = 'none';
 myVideo.style.display = 'block';
 function playVideo() { 
  myVideo.play(); 
+}
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
 } 
 playVideo()
 
